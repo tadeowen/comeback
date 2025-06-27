057e4c7f95d184dbdba7b0a63778fc5a040d3890
Kenaz79 <blessedkentacion@gmail.com>
2025-06-27 13:26:24 +0300

    Fixed errors

--- lib/widgets/custom_bottom_nav.dart
+++ lib/widgets/custom_bottom_nav.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
-class CustomButtomNav extends StatelessWidget {
+
+class CustomBottomNav extends StatelessWidget {
   final int selectedIndex;
   final Function(int) onTap;
 
-  // ignore: use_super_parameters
   const CustomBottomNav({
-    Key? key,
+    super.key,
     required this.selectedIndex,
     required this.onTap,
-
-  }) : super(key: key);
+  });
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: selectedIndex,
-      onTap:onTap,
+      onTap: onTap,
       type: BottomNavigationBarType.fixed,
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.self_improvement), label: 'Prayer'),
-        BottomNavigationBarItem(icon: Icon(Icons.chat), label:'Chat'),
-        BottomNavigationBarItem(icon: Icon(Icons.video_library), label: 'Media'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
-
+        BottomNavigationBarItem(
+          icon: Icon(Icons.self_improvement),
+          label: 'Prayer',
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.video_library),
+          label: 'Media',
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
       ],
     );
   }
