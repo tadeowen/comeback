b6d63d89eaba6491a60e1ccdf496267cde19e2ca
Kenaz79 <blessedkentacion@gmail.com>
2025-06-27 13:32:05 +0300

    Removed errors

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,20 +1,16 @@
-
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context)
-  {
-    return Center(
-      child: Text(
-        'Home Screen',
-        style: Theme.of(context).textTheme.headline4,
+  const HomeScreen({super.key});
 
-      ),
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Home Screen', style: Theme.of(context).textTheme.headline4),
     );
   }
 }
 
 extension on TextTheme {
-  get headline4 => null;
+  Null get headline4 => null;
 }
